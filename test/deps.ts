c76f2b471e448923999e29cf79ed3a3b07592387
asos-craigmorten <craig.morten@asos.com>
2020-06-29 12:22:02 +0100

    feat: upgrade test deps

--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
 export { expect } from "https://deno.land/x/expect@285caf/mod.ts";
-export * as Opine from "https://deno.land/x/opine@0.12.0/mod.ts";
-export * as OpineTypes from "https://deno.land/x/opine@0.12.0/src/types.ts";
+export * as Opine from "https://deno.land/x/opine@0.13.0/mod.ts";
+export * as OpineTypes from "https://deno.land/x/opine@0.13.0/src/types.ts";
 export * as Oak from "https://deno.land/x/oak@v5.3.1/mod.ts";
 export { dirname, join } from "https://deno.land/std@0.59.0/path/mod.ts";
