031121765dbe9b6ae71b7a99a5ca9e8faf34b7c2
asos-craigmorten <craig.morten@asos.com>
2020-08-21 11:18:40 +0100

    feat: 2.2.0

--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
+export { dirname, join } from "https://deno.land/std@0.65.0/path/mod.ts";
 export { expect } from "https://deno.land/x/expect@v0.2.1/mod.ts";
-export * as Opine from "https://deno.land/x/opine@0.20.0/mod.ts";
-export * as OpineTypes from "https://deno.land/x/opine@0.20.0/src/types.ts";
-export * as Oak from "https://deno.land/x/oak@v6.0.1/mod.ts";
-export { dirname, join } from "https://deno.land/std@0.63.0/path/mod.ts";
+export * as Opine from "https://deno.land/x/opine@0.21.2/mod.ts";
+export * as OpineTypes from "https://deno.land/x/opine@0.21.2/src/types.ts";
+export * as Oak from "https://deno.land/x/oak@v6.0.2/mod.ts";
