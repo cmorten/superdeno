223334e2007c0f18b6dcd9484d1bf131df6e57df
Craig Morten <46491566+asos-craigmorten@users.noreply.github.com>
2020-08-21 11:21:45 +0100

    [NO-ISSUE] Bump Deno and lib versions (#14)

--- deps.ts
+++ deps.ts
@@ -4,11 +4,11 @@ export {
   HTTPOptions,
   Server,
   Response,
-} from "https://deno.land/std@0.63.0/http/server.ts";
-export { STATUS_TEXT } from "https://deno.land/std@0.63.0/http/http_status.ts";
-export { assertEquals } from "https://deno.land/std@0.63.0/testing/asserts.ts";
-export { methods } from "https://deno.land/x/opine@0.20.0/src/methods.ts";
-export { mergeDescriptors } from "https://deno.land/x/opine@0.20.0/src/utils/mergeDescriptors.ts";
+} from "https://deno.land/std@0.65.0/http/server.ts";
+export { STATUS_TEXT } from "https://deno.land/std@0.65.0/http/http_status.ts";
+export { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export { methods } from "https://deno.land/x/opine@0.21.2/src/methods.ts";
+export { mergeDescriptors } from "https://deno.land/x/opine@0.21.2/src/utils/mergeDescriptors.ts";
 // TODO: import these as production modules
 export { default as util } from "https://dev.jspm.io/npm:util@0.12.3";
 export { default as superagent } from "https://dev.jspm.io/npm:superagent@5.3.1";
