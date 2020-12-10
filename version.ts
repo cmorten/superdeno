f128728bf6abe817aeea7782f96925f734b154a3
cmorten <craig.morten@hotmail.co.uk>
2020-12-10 22:51:49 +0000

    feat: support 1.6.0

--- version.ts
+++ version.ts
@@ -1,9 +1,9 @@
 /** 
- * Version of Opine.
+ * Version of SuperDeno.
  */
-export const VERSION: string = "2.4.1";
+export const VERSION: string = "2.5.0";
 
 /**
  * Supported versions of Deno.
  */
-export const DENO_SUPPORTED_VERSIONS: string[] = ["1.5.4"];
+export const DENO_SUPPORTED_VERSIONS: string[] = ["1.6.0"];
