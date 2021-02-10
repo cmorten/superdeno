de3e34189c46d49f87292ddd96a0e5e121fb6376
cmorten <craig.morten@hotmail.co.uk>
2021-02-10 21:21:05 +0000

    refactor: use JSPM production url

--- deps.ts
+++ deps.ts
@@ -8,5 +8,4 @@ export { STATUS_TEXT } from "https://deno.land/std@0.85.0/http/http_status.ts";
 export { assertEquals } from "https://deno.land/std@0.85.0/testing/asserts.ts";
 export { methods } from "https://deno.land/x/opine@1.1.0/src/methods.ts";
 export { mergeDescriptors } from "https://deno.land/x/opine@1.1.0/src/utils/mergeDescriptors.ts";
-// TODO: import these as production modules
-export { default as superagent } from "https://dev.jspm.io/npm:superagent@6.1.0";
+export { default as superagent } from "https://jspm.dev/superagent@6.1.0";
