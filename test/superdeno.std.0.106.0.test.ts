05d8784613c24405cd702cce2ddf51d9e2c9f6d1
cmorten <craig.morten@hotmail.co.uk>
2021-09-20 22:36:38 +0100

    feat: harden tests for windows

--- test/superdeno.std.0.106.0.test.ts
+++ test/superdeno.std.0.106.0.test.ts
@@ -4,27 +4,31 @@ import { superdeno } from "../mod.ts";
 
 describe("std: superdeno", () => {
   it("std: superdeno(server): should support `superdeno(server)`", async () => {
-    const server = serve("127.0.0.1:8080");
+    const server = serve("127.0.0.1:0");
 
-    (async () => {
+    const requestLoopPromise = (async () => {
       for await (const request of server) {
         await request.respond({ body: "hello" });
+        break;
       }
     })();
 
     await superdeno(server)
       .get("/")
       .expect("hello");
+
+    await requestLoopPromise;
   });
 
   it("std: superdeno(url): should support `superdeno(url)`", async () => {
-    const server = serve(":0");
+    const server = serve("127.0.0.1:0");
     const address = server.listener.addr as Deno.NetAddr;
     const url = `http://${address.hostname}:${address.port}`;
 
-    (async () => {
+    const requestLoopPromise = (async () => {
       for await (const request of server) {
         request.respond({ body: "hello" });
+        break;
       }
     })();
 
@@ -33,5 +37,6 @@ describe("std: superdeno", () => {
       .expect("hello");
 
     server.close();
+    await requestLoopPromise;
   });
 });
